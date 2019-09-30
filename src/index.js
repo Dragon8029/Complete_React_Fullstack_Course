c111561f178140a8dd0ec25dec0dd79d38d4fee0
Dragon8029 <dragon8029@gmail.com>
2019-09-30 06:53:14 -0700

    add basic code to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+
+const App = () =>{
+    return (
+        <div className="newElement">
+            <h1>Hello React!!</h1>
+            <div>hey</div>
+        </div>
+    )
+    
+}
+
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
