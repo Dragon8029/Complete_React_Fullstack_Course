52ac54159537c02e5e6630958e2ddf89290e4186
Dragon8029 <dragon8029@gmail.com>
2019-09-30 07:02:46 -0700

    add more code

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 const Header = () => {
-    return <div>Header</div>
+    return <div>Header from header.js</div>
 }
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// COMPONENTS
+import Header from './components/header'
 
 const App = () =>{
     return (
         <div>
-            Header
+            <Header/>
         </div>
     )
     
