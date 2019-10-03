35923982a0896de71222800bfffcc86536dd7cd4
Dragon8029 <dragon8029@gmail.com>
2019-10-03 06:52:29 -0700

    add more_on_classes code

---
+++ public/playground.js
@@ -0,0 +1,21 @@
+
+
+
+class React{
+    constructor(){
+        this.props = {
+
+        };
+    }
+}
+
+class Page extends React{
+    constructor(){
+        super();
+    }
+}
+
+const car = new Car();
+const ford = new Ford();
+
+console.log(ford.wheels);
