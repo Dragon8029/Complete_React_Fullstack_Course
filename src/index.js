128b881d16719a7f18dcf011e8bd465c8fc71999
Dragon8029 <dragon8029@gmail.com>
2019-10-07 06:05:48 -0700

    add more on styles code

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ class App extends Component {
             <div>
                 <Header/>
                 <NewsList news={this.state.news}>
-                    Hello Dudes!!!
+                    The news are: 
                 </NewsList>
             </div>
         )
